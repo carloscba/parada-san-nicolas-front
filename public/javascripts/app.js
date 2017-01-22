9cf21cec07a583bdd6a98e6922a43db846ffa900
carlos cordoba <carloscba1@hotmail.com>
2017-01-22 00:32:12 -0300

    - Mensaje de error - Loading

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,16 @@
 angular.module('app', []).controller('appCtrl', function($scope, $http) {
 
+	$scope.loading = true;
+
 	$http({
 		method: 'GET',
 		url: '/data'
 	}).then(function successCallback(response) {
-		
+		$scope.loading = false;
 		$scope.proximo = response.data[0].data[0]
 		$scope.moviles = response.data[0].data;
+
+
 
 	}, function errorCallback(response) {
 	
